257b76d909aec47a74dadcef2c3d42ed7399b462
accendro <accendro@gmail.com>
2026-04-29 01:53:20 +0600

    Add files via upload

--- global.js
+++ global.js
@@ -1,4 +1,6 @@
-        const API_URL = "https://script.google.com/macros/s/AKfycbyy96YQzl81yUzVW893x8fkYmt-kn8loEij5Tk817ejSlc_GUPFDdkM53YUWLPqv3IJyQ/exec";
+const STORE_ID = "GhorarDim_"; 
+
+const API_URL = "https://script.google.com/macros/s/AKfycbyy96YQzl81yUzVW893x8fkYmt-kn8loEij5Tk817ejSlc_GUPFDdkM53YUWLPqv3IJyQ/exec";
 
 const facebookPageUsername = "GhorarDimFibro";
 
